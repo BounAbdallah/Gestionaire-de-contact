051afe1441d9e921c41720d6bfaf09fa5275e0d4
Bouna_Abdallah <bigmabo61345@gmail.com>
2023-09-13 20:52:57 +0000

    Update

--- index.js
+++ index.js
@@ -1,20 +1,76 @@
+const contactModal = document.getElementById("contact-modal");
+const addContactBtn = document.getElementById("add-contact-btn");
+const closeContactModalBtn = document.getElementById("close-modal");
+const contactForm = document.getElementById("contact-form");
+const contactList = document.getElementById("contact-list");
 
+let contacts = [];
 
+addContactBtn.addEventListener("click", () => {
+    contactModal.style.display = "block";
+});
 
+closeContactModalBtn.addEventListener("click", () => {
+    contactModal.style.display = "none";
+});
 
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    const prenom = document.getElementById("prenom").value;
+    const nom = document.getElementById("nom").value;
+    const email = document.getElementById("email").value;
+    const numero = document.getElementById("numero").value;
 
-/*modal*/
-let modal = document.getElementById("contactModal")
-let modalButton =document.getElementById("addContactModalButton")
-let close = document.querySelector("close")
+    const newContact = {
+        prenom,
+        nom,
+        email,
+        numero
+    };
 
+    contacts.push(newContact);
 
+    contactForm.reset();
+    contactModal.style.display = "none";
 
-modalButton.onclick = function () {
-  modal.style.display = 'block'
+    displayContacts();
+});
+
+function displayContacts() {
+    contactList.innerHTML = "";
+
+    contacts.forEach((contact, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span>${contact.prenom} ${contact.nom}</span>
+            <span>${contact.email}</span>
+            <span>${contact.numero}</span>
+            <button onclick="editContact(${index})">Modifier</button>
+            <button onclick="deleteContact(${index})">Supprimer</button>
+        `;
+        contactList.appendChild(li);
+    });
 }
 
-close.onclick = function () {
-  modal.style.display = 'none'
+function editContact(index) {
+    const contact = contacts[index];
+
+    document.getElementById("prenom").value = contact.prenom;
+    document.getElementById("nom").value = contact.nom;
+    document.getElementById("email").value = contact.email;
+    document.getElementById("numero").value = contact.numero;
+
+    contactModal.style.display = "block";
 }
+
+function deleteContact(index) {
+    const confirmation = confirm("Voulez-vous vraiment supprimer ce contact ?");
+
+    if (confirmation) {
+        contacts.splice(index, 1);
+        displayContacts();
+    }
+}
+
+displayContacts();
